e808d99104abce8cfbcfc73b8855360cbbc1a97f
Matvdg <matvdg@me.com>
2016-06-20 20:30:06 -0400

    assets V2

--- PrepApp/TabBarViewController.swift
+++ PrepApp/TabBarViewController.swift
@@ -12,10 +12,19 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        for item in self.tabBar.items! as [UITabBarItem] {
-            if let image = item.selectedImage {
-                item.selectedImage = image.imageWithColor(Colors.prepapp).imageWithRenderingMode(.AlwaysOriginal)
-            }
+        for index in 0..<self.tabBar.items!.count {
+                switch index {
+                case 0:
+                    self.tabBar.items![0].selectedImage = UIImage(named: "homeSelected")!.imageWithColor(Colors.prepapp).imageWithRenderingMode(.AlwaysOriginal)
+                case 1:
+                    self.tabBar.items![1].selectedImage = UIImage(named: "trainingSelected")!.imageWithColor(Colors.prepapp).imageWithRenderingMode(.AlwaysOriginal)
+                case 2:
+                    self.tabBar.items![2].selectedImage = UIImage(named: "challengeSelected")!.imageWithColor(Colors.prepapp).imageWithRenderingMode(.AlwaysOriginal)
+                case 3:
+                    self.tabBar.items![3].selectedImage = UIImage(named: "duoSelected")!.imageWithColor(Colors.prepapp).imageWithRenderingMode(.AlwaysOriginal)
+                default:
+                    self.tabBar.items![4].selectedImage = UIImage(named: "otherSelected")!.imageWithColor(Colors.prepapp).imageWithRenderingMode(.AlwaysOriginal)
+                }
         }
         UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: Colors.prepapp], forState: .Selected)
     }
